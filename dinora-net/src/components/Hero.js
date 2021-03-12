ef18363ac60403c1929542d79cbcbcd4cc022f61
Eduardo Villa <edvilla@ucsc.edu>
2021-03-11 18:20:42 -0800

    Reformatted hero elemtns into divs, added responsive drop down buttons, cleaned up hero.css, added more relevant data to TempServer.json
    - We ended up completely rewriting the hero body components. We additionally turned this into a class component because it helped us pass states into the drop down buttons. You can find that the elements are now reactive to width, but they do not shift down.
    
    - We added drop down buttons. You can select options and the button title will change.
    
    - We modified the table database so that we could see more relevant dummy data that came from the MIRO.
    
    - We cleaned up Hiro.css to remove things that we did not use.
    
    @aziz158  pair programmed with me

--- dinora-net/src/components/Hero.js
+++ dinora-net/src/components/Hero.js
@@ -5,50 +5,68 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Hero() {
-  return (
-    <div className='hero-container'>
-        <div className='logo'>
-            dinora.net
-        </div>
-        <div className= 'text'>
-            Send money FREE. No fees. No exchange rate bs
-        </div>
-        <div className= 'text-content'>
-            Send money to your loved ones without fees and exchange rate losses. You send $100 your family receives $100
-        </div>
-        <div className= 'send-from-button'>
-            <DropdownButton id="dropdown-basic-button" title="Send From" variant='secondary'> 
-                <Dropdown.Item href="#/action-1">Russia</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">USA</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Uzbekistan</Dropdown.Item>
-            </DropdownButton>
-        </div>
-        <div className= 'send-to-button'>
-            <DropdownButton id="dropdown-basic-button" title="Send To" variant='secondary'> 
-                <Dropdown.Item href="#/action-1">Russia</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">USA</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Uzbekistan</Dropdown.Item>
-            </DropdownButton>
-        </div>
+class Hero extends React.Component {
 
-        <div className='btns-container'>
-            <div className='buttn'>
-                <Button variant="outline-light" block>you send $100</Button>
+    constructor() {
+        super();
+    
+        this.state = {
+          sendFromValue: "Send From",
+          sendToValue: "Send To"
+        }
+      }
+    
+    changeValueSendFrom(text) {
+        this.setState({sendFromValue: text})
+    }
+
+    changeValueSendTo(text) {
+        this.setState({sendToValue: text})
+    }
+
+    render() {
+        return(
+            <div className='hero-container'>
+                <div className='header-container'>
+                    <div className='logo'>
+                        dinora.net
+                </div>
             </div>
-            <div className='buttn'>
-                <Button variant="outline-light" block>they get $100</Button>
-            </div>
-            <div className='buttn'>
-                <Button variant="light" block>Get Started</Button>
+
+            <div className='body-container'>
+                <div className='pitch-container'>
+                    <h3>
+                        Send money FREE. No fees. No exchange rate bs
+                    </h3>
+                    <br></br>
+                    <h5>
+                        Send money to your loved ones without fees and exchange rate losses. You send $100 your family receives $100
+                    </h5>
+                </div>
+                <div className='send-container'>
+                    <div className='send-from-container'>
+                        <DropdownButton id="dropdown-basic-button" title={this.state.sendFromValue} variant='secondary'> 
+                            <Dropdown.Item as="button"><div onClick={(e) => this.changeValueSendFrom(e.target.textContent)}>Russia</div></Dropdown.Item>
+                            <Dropdown.Item as="button"><div onClick={(e) => this.changeValueSendFrom(e.target.textContent)}>USA</div></Dropdown.Item>
+                            <Dropdown.Item as="button"><div onClick={(e) => this.changeValueSendFrom(e.target.textContent)}>Uzbekistan</div></Dropdown.Item>
+                        </DropdownButton>
+                    </div>
+                    <div className='send-to-container'>
+                        <DropdownButton id="dropdown-basic-button" title={this.state.sendToValue} variant='secondary'> 
+                            <Dropdown.Item as="button"><div onClick={(e) => this.changeValueSendTo(e.target.textContent)}>Russia</div></Dropdown.Item>
+                            <Dropdown.Item as="button"><div onClick={(e) => this.changeValueSendTo(e.target.textContent)}>USA</div></Dropdown.Item>
+                            <Dropdown.Item as="button"><div onClick={(e) => this.changeValueSendTo(e.target.textContent)}>Uzbekistan</div></Dropdown.Item>
+                        </DropdownButton>
+                    </div>
+                </div>
+                <div className='actions-container'>
+                    <Button className='buttonSecondary'>Send $100</Button>
+                    <Button className='buttonSecondary'>Recieve $100</Button>
+                    <Button className='buttonPrimary'>Get Started</Button>
+                </div>
             </div>
         </div>
-    </div>
-  );
+        );
+    };
 }
-
-
-            
-            
-
 export default Hero;
